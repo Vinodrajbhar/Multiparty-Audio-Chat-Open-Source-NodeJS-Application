5021a34696f33b9e1c9ccda76bf242294837d3e4
Vinod Rajbhar <vinod.r@vcloudx.com>
2020-06-24 09:05:55 +0530

    removed logs

--- server/vcxroom.js
+++ server/vcxroom.js
@@ -13,8 +13,8 @@ var fs = require('fs')
 var vcxconfig = require('./vcxconfig')
 var vcxutil = require('./vcxutil')
 
-var log = require('../util/logger/logger').logger;
-var logger = log.getLogger('AppApi');
+// var log = require('../util/logger/logger').logger;
+// var logger = log.getLogger('AppApi');
 var vcxroom = {};
 
 var obj = {
--- server/vcxutil.js
+++ server/vcxutil.js
@@ -13,8 +13,8 @@ var btoa = require('btoa');
 var http = require('http');
 var https = require('https');
 var vcxconfig = require('./vcxconfig');
-var log = require('../util/logger/logger').logger;
-var logger = log.getLogger('AppApi');
+// var log = require('../util/logger/logger').logger;
+// var logger = log.getLogger('AppApi');
 var vcxutil = {};
 
 
@@ -33,11 +33,11 @@ vcxutil.getBasicAuthToken = function () {
 // Function: To connect to Enablex Server API Service
 
 vcxutil.connectServer = function (options, data, callback) {
-    logger.info("REQ URI:- " + options.method + " " + options.host + ":" + options.port + options.path);
-    logger.info("REQ PARAM:- " + data);
+    console.log("REQ URI:- " + options.method + " " + options.host + ":" + options.port + options.path);
+    console.log("REQ PARAM:- " + data);
     var request = https.request(options, function (res) {
         res.on('data', function (chunk) {
-            logger.info("RESPONSE DATA:- " + chunk);
+            console.log("RESPONSE DATA:- " + chunk);
             if (chunk.result === 0) {
                 callback('success', JSON.parse(chunk));
             }
@@ -47,7 +47,7 @@ vcxutil.connectServer = function (options, data, callback) {
         });
     });
     request.on('error', function (err) {
-        logger.info("RESPONSE ERROR:- " + JSON.stringify(err));
+        console.log("RESPONSE ERROR:- " + JSON.stringify(err));
 
     });
     if (data == null)
